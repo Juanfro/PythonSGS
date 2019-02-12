065bdaed57b1537777f1a9b9c417c9da1a566def
Juanfro <juanfropro@gmail.com>
2019-02-12 12:43:48 +0100

    Inicio Ej 3

---
+++ pythonGIT/src/Practica3.py
@@ -0,0 +1,26 @@
+'''
+Created on 8 feb. 2019
+
+@author: Juan Antonio Rodríguez
+'''
+from random import random, randint
+
+
+""" Ejercicio 1
+Escribe un programa que te permita jugar a una versión simplificada del juego Master Mind.
+El juego consistirá en adivinar una cadena de números distintos. Al principio, el programa
+debe pedir la longitud de la cadena (de 2 a 9 cifras). Después el programa debe ir pidiendo que
+intentes adivinar la cadena de números. En cada intento, el programa informará de cuántos
+números han sido acertados (el programa considerará que se ha acertado un número si
+coincide el valor y la posición).
+"""
+
+def mastermind():
+    longitud = int(input("Elige la longitud de la lista de numeros"))
+    lista=[]
+    for x in range(longitud):
+        lista.append(randint(0,9))
+        
+    print(lista)
+    
+mastermind()
